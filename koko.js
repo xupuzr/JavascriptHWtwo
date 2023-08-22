ac0c73a38d1e90e6dd5886ce8dd42ce8459cf462
xupuzurchik <lopikop008@gmail.com>
2023-08-22 21:58:33 +0500

    xupuzurchik

---
+++ koko.js
@@ -0,0 +1,5 @@
+var chislo1 = +prompt('Введите первое число')
+var chislo2 = +prompt('Введите второе число')
+var chislo3 = +prompt('Введите третье число')
+
+console.log('Среднее число = ' + (chislo1 + chislo2 + chislo3) / 3  );
